c6eb3a9e12e76c7d887069797868d7c82068e650
romulobugas <romulo.mauriz@gmail.com>
2020-11-02 01:44:54 -0300

    MVC 0.1.1

---
+++ model/Conexao.php
@@ -0,0 +1,28 @@
+<?php
+
+class Conexao {
+    private static $conexao;
+
+    private function _construct()
+    {
+
+    }
+    
+    public static function getInstance(){
+
+        if (is_null(self::$conexao)) {
+            # Conexão com o banco...
+            self::$conexao = new \PDO('mysql:host=localhost;
+                                       port-3306;
+                                       dbname=crud',
+                                       'teste',
+                                       '6&@7ZtPlqXLa@mFe');
+            self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            self::$conexao->exec('set names utf8mb4');
+            echo 'Show de bola, pode ir dormir';
+        }
+        return self::$conexao;
+    }
+}
+
+?>
